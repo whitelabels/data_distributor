9c8f7930555015cd593e39869eded6966bb8236b
TorUlr <torben@ulricht.com>
2022-06-10 14:14:06 +0200

    Updated require ''

--- lib/data_distributor.rb
+++ lib/data_distributor.rb
@@ -1,6 +1,6 @@
-require_relative 'data_distributor/authentication/no_data_distributor/authentication'
-require_relative 'data_distributor/authentication/credentials'
-require_relative 'data_distributor/bbr/building'
-require_relative 'data_distributor/bbr/unit'
-require_relative 'data_distributor/bbr/client'
+require 'data_distributor/authentication/no_data_distributor/authentication'
+require 'data_distributor/authentication/credentials'
+require 'data_distributor/bbr/building'
+require 'data_distributor/bbr/unit'
+require 'data_distributor/bbr/client'
 
