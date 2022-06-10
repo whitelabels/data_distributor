b000685f3ae25268da3feee5cc0acf5a0c8949bc
TorUlr <torben@ulricht.com>
2022-06-10 14:00:17 +0200

    Missing requires added

---
+++ lib/data_distributor.rb
@@ -0,0 +1,6 @@
+require_relative 'authentication/no_authentication'
+require_relative 'authentication/credentials'
+require_relative 'bbr/building'
+require_relative 'bbr/unit'
+require_relative 'bbr/client'
+
