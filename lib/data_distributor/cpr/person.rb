07c60d514d67e29c76f972c279d7913d38248b1d
TorUlr <torben@ulricht.com>
2022-06-16 09:34:40 +0200

    Adds age and address lookup

--- lib/data_distributor/cpr/person.rb
+++ lib/data_distributor/cpr/person.rb
@@ -44,8 +44,6 @@ module DataDistributor
             else
               year += 1900
             end
-          else
-            # type code here
           end
 
           Date.new(year, month, day)
@@ -65,44 +63,11 @@ module DataDistributor
 
       def address
         if data.dig(:Person, :Adresseoplysninger, :CprAdresse).nil?
-          #if data[:Person][:Adresseoplysninger][:CprAdresse].nil?
           nil
         else
           Address.new(data[:Person][:Adresseoplysninger][:CprAdresse])
         end
-
-
       end
-
-      def street
-        begin
-          street_name = result['Personer'][0]['Person']['Adresseoplysninger']['CprAdresse']['vejadresseringsnavn']
-          house_number = result['Personer'][0]['Person']['Adresseoplysninger']['CprAdresse']['husnummer'].to_i
-          zip_code = result['Personer'][0]['Person']['Adresseoplysninger']['CprAdresse']['postnummer']
-          city = result['Personer'][0]['Person']['Adresseoplysninger']['CprAdresse']['postdistrikt']#bynavn']
-
-          street_address = street_name + ' ' + house_number.to_s
-        rescue
-          street_address = ''
-          zip_code = ''
-          city = ''
-        end
-
-        return street_address, zip_code, city
-      end
-
-
-=begin
-  def initialize(is_valid, full_name, street, zip, city, age, under_guardianship)
-    @valid_cpr_number = is_valid
-    @full_name = full_name
-    @street = street
-    @zip = zip
-    @city = city
-    @age = age
-    @under_guardianship = under_guardianship
-  end
-=end
     end
   end
 end
