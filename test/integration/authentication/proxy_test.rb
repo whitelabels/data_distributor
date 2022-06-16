db19bfbb09d2a168d7af29dcfa6568a8d4782536
TorUlr <torben@ulricht.com>
2022-06-16 10:09:55 +0200

    Test proxy authentication

---
+++ test/integration/authentication/proxy_test.rb
@@ -0,0 +1,43 @@
+require_relative "../../test_helper"
+
+describe DataDistributor::Authentication::Proxy do
+  it "can apply the middleware to the connection" do
+    connection = Minitest::Mock.new
+    connection.expect(:use, nil, [DataDistributor::Authentication::Proxy::Middleware, proxy_host: URI("http://example.com:9000")])
+    DataDistributor::Authentication::Proxy.new(proxy_host: "http://example.com:9000").apply(connection)
+    connection.verify
+  end
+
+  it "will change the scheme" do
+    env = Minitest::Mock.new
+    env.expect(:url, URI("http://example.com"))
+    env.expect(:url=, nil) { |url| _(url.scheme).must_equal("https")}
+    app = Minitest::Mock.new
+    app.expect(:call, nil, [env])
+    DataDistributor::Authentication::Proxy::Middleware.new(app, proxy_host: URI("https://example.com")).call(env)
+
+    env.verify
+  end
+
+  it "will change the host" do
+    env = Minitest::Mock.new
+    env.expect(:url, URI("http://example.com"))
+    env.expect(:url=, nil) { |url| _(url.host).must_equal("example.org")}
+    app = Minitest::Mock.new
+    app.expect(:call, nil, [env])
+    DataDistributor::Authentication::Proxy::Middleware.new(app, proxy_host: URI("http://example.org")).call(env)
+
+    env.verify
+  end
+
+  it "will change the port" do
+    env = Minitest::Mock.new
+    env.expect(:url, URI("http://example.com:8000"))
+    env.expect(:url=, nil) { |url| _(url.port).must_equal(8009)}
+    app = Minitest::Mock.new
+    app.expect(:call, nil, [env])
+    DataDistributor::Authentication::Proxy::Middleware.new(app, proxy_host: URI("http://example.com:8009")).call(env)
+
+    env.verify
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,7 @@ require 'minitest/autorun'
 require 'uri'
 require 'securerandom'
 require 'vcr'
-
-require_relative '../lib/data_distributor/authentication/no_authentication'
-require_relative '../lib/data_distributor/authentication/credentials'
-require_relative '../lib/data_distributor/bbr/building'
-require_relative '../lib/data_distributor/bbr/unit'
-require_relative '../lib/data_distributor/bbr/client'
+require 'data_distributor'
 
 VCR.configure do |config|
   config.cassette_library_dir = 'test/cassettes'
